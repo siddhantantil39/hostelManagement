446c694966d9eda3ff18d4fb3543a4fb733ea6ef
Rajan Mishra <rajuengir@gmail.com>
2022-04-26 11:04:00 +0530

    before pulling the Antil frontend survey code

--- frontend/hostelmanagement/src/components/login/LoginPage.jsx
+++ frontend/hostelmanagement/src/components/login/LoginPage.jsx
@@ -55,7 +55,7 @@ const Login = () => {
 						/>
 						{error && <div className={styles.error_msg}>{error}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sing In
+							Sign In
 						</button>
 					</form>
 				</div>
