4683dd75bf030ad4721c792bacc90b1f4c09c4d5
Siddhant <siddhantantil39@gmail.com>
2022-04-26 10:41:37 +0530

    Updated survey form minor change

--- frontend/hostelmanagement/src/App.js
+++ frontend/hostelmanagement/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
 	return (
 		<Routes>
-			{<Route path="/" exact element={<Main />} />}
+			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
